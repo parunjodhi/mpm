ffaefed3ca62f8ed5fa8c1258d68507aa2ee499a
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-11-10 18:17:48 -0500

    wrangling pkge

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,21 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+library(tidyverse)
+owid_ghg <- read_csv("data-raw/owid-co2-data.csv")
+
+# Y (the GHG) total_ghg / coal_co2 / methane / population / gdp
+# X (years) 1940 - 2020
+# Key (country) country / U.S is the default
+
+owid_ghg <- owid_ghg %>% select (year, country, population, gdp, total_ghg, coal_co2, methane)
+
+# 2022-11-08 creating a data set
+# filtering for north american countries
+# filtering by years
+# completing the description
+# create a push of all edits for Mack and Parunjodhi to view
+# 2022-09-22 inside collaborator questions
+
+
+
+usethis::use_data(owid_ghg, overwrite = TRUE)
