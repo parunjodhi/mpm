3e495fcd22073adaf54ef2db6f3de6174e84b691
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-11-10 18:19:20 -0500

    adding 'to do items' for data wrangling in DATASET.R, will return to complete this over the weekend

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,15 +7,20 @@ owid_ghg <- read_csv("data-raw/owid-co2-data.csv")
 # X (years) 1940 - 2020
 # Key (country) country / U.S is the default
 
-owid_ghg <- owid_ghg %>% select (year, country, population, gdp, total_ghg, coal_co2, methane)
+owid_ghg <- owid_ghg %>%
+  select (year, country, population, gdp, total_ghg, coal_co2, methane) %>%
+  filter(country == "North America" | country == "Canada" | country == "Mexico" | country == "Bermuda")
 
-# 2022-11-08 creating a data set
+view(owid_ghg)
+
+# TO DO LIST:
 # filtering for north american countries
 # filtering by years
 # completing the description
 # create a push of all edits for Mack and Parunjodhi to view
+
+# RESOURCES:
+# 2022-11-08 creating a data set
 # 2022-09-22 inside collaborator questions
-
-
 
 usethis::use_data(owid_ghg, overwrite = TRUE)
