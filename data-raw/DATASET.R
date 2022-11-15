f7cfa211e436c8232268f7fd6d483d38d2e23fa0
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-11-14 22:41:02 -0500

    wrangling continuation - filtering by countries in North American and years between 1940 and 2020

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,26 +1,20 @@
 ## code to prepare `DATASET` dataset goes here
 
 library(tidyverse)
+library(dplyr)
 owid_ghg <- read_csv("data-raw/owid-co2-data.csv")
-
-# Y (the GHG) total_ghg / coal_co2 / methane / population / gdp
-# X (years) 1940 - 2020
-# Key (country) country / U.S is the default
 
 owid_ghg <- owid_ghg %>%
   select (year, country, population, gdp, total_ghg, coal_co2, methane) %>%
-  filter(country == "North America" | country == "Canada" | country == "Mexico" | country == "Bermuda")
-
-view(owid_ghg)
-
-# TO DO LIST:
-# filtering for north american countries
-# filtering by years
-# completing the description
-# create a push of all edits for Mack and Parunjodhi to view
-
-# RESOURCES:
-# 2022-11-08 creating a data set
-# 2022-09-22 inside collaborator questions
+  filter(country == "United States" | country == "Mexico" | country == "Canada" |
+           country == "Bermuda" | country == "Guatemala" | country == "Cuba" | country == "Haiti" |
+           country == "Dominican Republic" | country == "Honduras" | country == "Nicaragua" |
+           country == "El Salvador" | country =="Costa Rica" | country == "Panama" | country == "Jamaica" |
+           country == "Trinidad and Tobago" | country == "Belize" | country == "Bahamas" |
+           country == "Barbados" | country == "Saint Lucia" | country == "Grenada" |
+           country == "Saint Vincent and the Grenadines" |
+           country == "Antigua and Barbuda" | country == "Dominica" | country == "Saint Kitts and Nevis" |
+           country == "North America") %>%
+  filter(!(year <= "1939"))
 
 usethis::use_data(owid_ghg, overwrite = TRUE)
