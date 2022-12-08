e1352e9c08cc1a38c3e2065ae4b73733971eb417
Mack Case <hcase@smith.edu>
2022-12-08 10:37:17 -0500

    making dalys_function more intuitive in its output

--- R/dalys_function.R
+++ R/dalys_function.R
@@ -1,5 +1,5 @@
 
-#' Title
+#' Predict Disability Adjusted Life Years and Total Greenhouse Gas emissions in million tonnes for a specific country in a future year
 #'
 #' @param pred_year
 #' @param nation
@@ -29,12 +29,24 @@ dalys_predict <- function(pred_year, nation = "United States"){
 
   dalys_model_air <- lm(air_pollution ~ year, data = all_data)
   ghg_model <- lm(total_ghg ~ year, data = all_data)
+  pop_model <- lm(population ~ year, data = all_data)
 
 
-  pred_value_air <- predict(dalys_model_air, data.frame(year = pred_year))
-  pred_value_ghg <- predict(ghg_model, data.frame(year = pred_year))
 
+  pred_value_air <- round(predict(dalys_model_air, data.frame(year = pred_year), digits = 3))
+  pred_value_ghg <- round(predict(ghg_model, data.frame(year = pred_year)), digits = 3)
+  pred_value_pop <- round(predict(pop_model, data.frame(year = pred_year)), digits = 3)
 
-  paste0("The Total DALYs predicted for ", nation, " for the year ", pred_year, " is ", pred_value_air, " and the total predicted ghg is ", pred_value_ghg, ".")
+  dalys_per_person <- pred_value_air/pred_value_pop
+
+  warning(paste("This prediction is based off of a linear regression model, even if the data might not follow a linear pattern over the years. This is for estimation purposes only."))
+
+  paste0("The Total DALYs predicted for ", nation, " for the year ", pred_year, " is ",
+         pred_value_air, " total years. Each person will suffer from a loss in ",
+         dalys_per_person, " years. The total predicted ghg is ", pred_value_ghg, " million tonnes.")
 
 }
+
+
+
+
