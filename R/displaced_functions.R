f4ced028851a34e8dea23941fb01b6105a7cd474
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-12-17 14:11:29 -0500

    adding global var

--- R/displaced_functions.R
+++ R/displaced_functions.R
@@ -1,41 +1,8 @@
-#' Distribution of the range of people displaced, in a specific country, by natural disasters and climate change
+globalVariables(c("displaced_count"))
+
+  #' Distribution of the range of people displaced, in a specific country, by natural disasters and climate change
 #'
 #' This function visualizes the distribution of internally displaced people in a specific country (default: USA)
-#'
-#' @param nation Name of country (default: United States)
-#'
-#' @return A histogram demonstrating the amount of people displaced based on the country selected
-#' @export
-#'
-#' @examples
-#' displacement_histogram()
-#' displacement_histogram("Haiti")
-#' @import ggplot2
-#' @import tidyr
-#' @import dplyr
-displacement_histogram() <-
-
-  function(nation = "United States") {
-
-  histo_data <-
-    displaced_by_disaster %>%
-    filter(country == nation) %>%
-    select(country, displaced_count)
-
-  displaced_histogram <-
-    ggplot(histo_data, aes(x = displaced_count)) +
-    geom_histogram(colour = "#C04000") +
-    scale_x_continuous(labels = displaced_count) +
-    labs(
-      x = "Internally Displaced People",
-      y = "count",
-      title = paste("Distribution of Displaced Individuals in ", nation, "from 2008-2021"))
-
-  return(displaced_histogram)}
-
-#' Faceted bar plot visualizing internally displacements individuals and years for three countries in North America
-#'
-#' This function visualizing the relationship between displacements and years for three countries in North Americ (default: USA)
 #'
 #' @param nation Name of country (default: United States)
 #'
@@ -52,18 +19,17 @@ displacement_histogram <-
 
   function(nation = "United States") {
 
-    histo_data <-
-      displaced_by_disaster %>%
-      filter(country == nation) %>%
-      select(country, displaced_count)
+  histo_data <-
+    displaced_by_disaster %>%
+    filter(country == nation) %>%
+    select(country, displaced_count)
 
-    displaced_histogram <-
-      ggplot(histo_data, aes(x = displaced_count)) +
-      geom_histogram(colour = "#C04000") +
-      scale_x_continuous(labels = displaced_count) +
-      labs(
-        x = "Internally Displaced People",
-        y = "count",
-        title = paste("Distribution of Displaced Individuals in ", nation, "from 2008-2021"))
+  displaced_histogram <-
+    ggplot(histo_data, aes(x = displaced_count)) +
+    geom_histogram(fill = "#C04000") +
+    labs(
+      x = "Internally Displaced People",
+      y = "count",
+      title = paste("Distribution of Displaced Individuals in ", nation, "from 2008-2021"))
 
-    return(displaced_histogram)}
+  return(displaced_histogram)}
