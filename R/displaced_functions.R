0a64930582336bd870804fa02234c0d88fd2e3d4
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-12-17 22:02:39 -0500

    adding linegraph

--- R/displaced_functions.R
+++ R/displaced_functions.R
@@ -1,6 +1,6 @@
-globalVariables(c("displaced_count"))
+globalVariables(c("displaced_count", "year", "country"))
 
-  #' Distribution of the range of people displaced, in a specific country, by natural disasters and climate change
+#' Distribution of the range of people displaced, in a specific country, by natural disasters and climate change
 #'
 #' This function visualizes the distribution of internally displaced people in a specific country (default: USA)
 #'
@@ -10,12 +10,11 @@ globalVariables(c("displaced_count"))
 #' @export
 #'
 #' @examples
-#' displacement_histogram()
-#' displacement_histogram("Haiti")
+#' displaced_histogram()
+#' displaced_histogram("Dominican Republic")
 #' @import ggplot2
 #' @import tidyr
-#' @import dplyr
-displacement_histogram <-
+displaced_histogram <-
 
   function(nation = "United States") {
 
@@ -33,3 +32,36 @@ displacement_histogram <-
       title = paste("Distribution of Displaced Individuals in", nation, "from 2008-2021"))
 
   return(displaced_histogram)}
+
+#' Line graph comparing the amount of displaced individuals over 13 years for two regions (a country and North America, or two countries)
+#'
+#' This function ... specific country (default: USA)
+#'
+#' @param nation Name of country (default: United States)
+#'
+#' @return A faceted-line graph demonstrating the amount of people displaced from 2008 to 2021 in two countries, or one country and North America
+#' @export
+#'
+#' @examples
+#' displaced_linegraph ()
+#' displaced_linegraph("Haiti")
+#' @import ggplot2
+#' @import tidyr
+displaced_linegraph <-
+
+  function(nation = "United States") {
+
+    trends_data <-
+      displaced_by_disaster %>%
+      filter(country == nation) %>%
+      select(country, year, displaced_count)
+
+    displaced_linegraph <-
+      ggplot(trends_data, aes(x = year, y = displaced_count)) +
+      geom_line(fill="#800000") +
+      labs(
+        x = "Year",
+        y = "Amount of Displaced Individuals",
+        title = paste("Internally Displaced People Displaced Individuals in", nation, "from 2008-2021"))
+
+    return(displaced_linegraph)}
