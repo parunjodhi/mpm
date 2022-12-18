f4b51169587fcc1f9c061a157858eb3dc955ec24
Michel Ruiz-Fuentes <mruizfuentes@smith.edu>
2022-12-18 15:43:35 -0500

    editing man file

--- R/displaced_functions.R
+++ R/displaced_functions.R
@@ -6,7 +6,7 @@ globalVariables(c("displaced_count", "year", "country"))
 #'
 #' @param nation Name of country (default: United States)
 #'
-#' @return A histogram demonstrating the amount of people displaced based on the country selected
+#' @return A histogram demonstrating the amount of people displaced based on the country selected from 2008 to 2021
 #' @export
 #'
 #' @examples
@@ -26,8 +26,9 @@ displaced_histogram <-
 
   histo_displaced <-
     ggplot(histo_data, aes(x = displaced_count)) +
-    geom_histogram(fill="#69b3a2", color="#e9ecef", alpha=0.9, bins=20) +
+    geom_histogram(fill="#7D3C98", color="#CE93D8", alpha=0.9, size=1, bins=20) +
     scale_x_continuous(labels = comma) +
+    theme(axis.text.x = element_text(angle=60)) +
     labs(
       x = "Internally Displaced People",
       y = "count",
@@ -35,13 +36,13 @@ displaced_histogram <-
 
   return(histo_displaced)}
 
-#' Internally Displaced Individuals Over Thirteen Years (2008-2021)
+#' Internally Displaced Individuals Line Graph (2008-2021)
 #'
-#' This function produces a line graph comparing the amount of displaced individuals over 13 years for two regions (a country and North America, or two countries)(default: USA)
+#' This function produces a line graph depicting the toal amount of displaced individuals in a given country (default: USA)
 #'
 #' @param nation Name of country (default: United States)
 #'
-#' @return A faceted-line graph demonstrating the amount of people displaced from 2008 to 2021 in two countries, or one country and North America
+#' @return A line graph and area chart demonstrating the amount of people displaced from 2008 to 2021
 #' @export
 #'
 #' @examples
@@ -60,13 +61,49 @@ displaced_linegraph <-
       select(country, year, displaced_count)
 
     line_displaced <-
-      ggplot(trends_data, aes(x = year, y = displaced_count)) +
-      geom_line() + scale_color_brewer(palette="Accent") +
+      ggplot(trends_data, aes(x = year, y = displaced_count, fill=country)) +
+      geom_line(colour = "#E3963E", size=2) +
       scale_y_continuous(labels = comma) +
+      theme(axis.text.x = element_text(angle=60)) +
       labs(
         x = "Year",
         y = "Total Number of Displaced Individuals",
-        title = paste("Internally Displaced Individuals in", nation, "\nfrom 2008-2021"))
+        title = paste("Line Graph of Displaced Individuals inn", nation, "\nfrom 2008-2021"))
 
     return(line_displaced)}
 
+#' Internally Displaced Individuals Area Chart (2008-2021)
+#'
+#' This function produces an area chart visualizing how the amount of displaced individuals over 13 years for a given country (default: USA)
+#'
+#' @param nation Name of country (default: United States)
+#'
+#' @return An area graph and area chart demonstrating the amount of people displaced from 2008 to 2021
+#' @export
+#'
+#' @examples
+#' displaced_areachart ()
+#' displaced_areachart("Nicaragua")
+#' @import ggplot2
+#' @import tidyr
+#' @import scales
+displaced_areachart <-
+
+  function(nation = "United States") {
+
+    area_data <-
+      displaced_by_disaster %>%
+      filter(country == nation) %>%
+      select(country, year, displaced_count)
+
+    area_displaced <-
+      ggplot(area_data, aes(x = year, y = displaced_count, fill=country)) +
+      geom_area(fill = "#097969", color="#82E0AA", size=2) +
+      scale_y_continuous(labels = comma) +
+      theme(axis.text.x = element_text(angle=60)) +
+      labs(
+        x = "Year",
+        y = "Total Number of Displaced Individuals",
+        title = paste("Area Chart of Displaced Individuals in", nation, "\nfrom 2008-2021"))
+
+    return(area_displaced)}
